4b43d5eaf53da649da4d10f7bd8762e96d9fb033
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-10-31 06:43:22 +0100

    test deadlock issue

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -17,7 +17,7 @@ func TestTransferTx(t *testing.T) {
 	fmt.Println(" >> before:", account1.Balance, account2.Balance)
 
 	// run n concurrent transfer transactions
-	n := 10
+	n := 1
 	amount := int64(10)
 	errs := make(chan error)
 
@@ -118,5 +118,60 @@ func TestTransferTx(t *testing.T) {
 
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+
+}
+
+func TestTransferTxDeadLock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	fmt.Println(" >> before:", account1.Balance, account2.Balance)
+
+	// run n concurrent transfer transactions
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountId := account2.ID
+		toAccountId := account1.ID
+
+		if i%2 == 1 {
+			fromAccountId = account1.ID
+			toAccountId = account2.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountId,
+				ToAccountID:   toAccountId,
+				Amount:        amount,
+			})
+			errs <- err
+
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount1)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount2)
+
+	fmt.Println(" >> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
 
 }
