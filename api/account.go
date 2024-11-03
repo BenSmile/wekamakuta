ec47ade1ce29e83a42903a4953d66c86e2fdcf63
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-11-03 13:50:38 +0100

    create user api

---
+++ api/account.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/bensmile/wekamakuta/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (s *Server) createAccount(ctx *gin.Context) {
+
+	var req createAccountRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := s.store.CreateAccount(ctx, args)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
