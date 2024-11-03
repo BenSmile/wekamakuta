5bf4d6365cccc2a0ac93ebcf4a1c4ffedd4ff4b2
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-11-03 14:49:24 +0100

    get all accounts and account by id APIs

--- api/account.go
+++ api/account.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/bensmile/wekamakuta/db/sqlc"
@@ -34,5 +36,58 @@ func (s *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	ctx.JSON(http.StatusCreated, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id"  binding:"required,min=1"`
+}
+
+func (s *Server) getAccount(ctx *gin.Context) {
+
+	var req getAccountRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := s.store.GetAccount(ctx, req.ID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK, account)
+
+}
+
+type listAccountsRequest struct {
+	PageId   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (s *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageId - 1) * req.PageSize,
+	}
+	accounts, err := s.store.ListAccounts(ctx, args)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, accounts)
 }
