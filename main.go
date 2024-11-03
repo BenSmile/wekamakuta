eff34a09cdf5a8c7fd2ff75e358c1341693c3a71
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-11-03 15:30:49 +0100

    read db configuration from config in main.go

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/bensmile/wekamakuta/api"
 	db "github.com/bensmile/wekamakuta/db/sqlc"
+	"github.com/bensmile/wekamakuta/db/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -26,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err := server.Start(serverAddress); err != nil {
+	if err := server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start the server:", err)
 	}
 }
