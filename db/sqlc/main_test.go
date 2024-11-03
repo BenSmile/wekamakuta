97bed224ad7a7abd9d791e98a45cf6862d7f4643
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-11-03 14:50:21 +0100

    entries & tranfers code generated + test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSrouce = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var (
@@ -21,7 +21,7 @@ var (
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSrouce)
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("we cannot connect to the db", err)
 	}
---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransfer(t *testing.T) {
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	args := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        0,
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), args)
+
+	require.NoError(t, err)
+
+	require.Equal(t, account1.ID, transfer.FromAccountID)
+	require.Equal(t, account2.ID, transfer.ToAccountID)
+	require.NotEmpty(t, transfer.CreatedAt)
+}
