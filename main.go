4493711cb84c5b13a34b5a03de77b618573b96d5
BenSmile <benjkafirongo@gmail.com>
2024-11-23 11:45:40 +0100

    Ft/swagger docs (#11)
    * add session migrations & sqlc codes
    
    * refresh token implemented
    
    * update make to generate swagger docs
    
    * add swagger docs
    
    * add swagger
    
    ---------
    
    Co-authored-by: Smile-Benjamin <Smile.Benjamin@vodacom.cd>

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 	"github.com/bensmile/wekamakuta/api"
 	db "github.com/bensmile/wekamakuta/db/sqlc"
 	"github.com/bensmile/wekamakuta/db/util"
+	_ "github.com/bensmile/wekamakuta/doc/statik"
 	"github.com/bensmile/wekamakuta/gapi"
 	"github.com/bensmile/wekamakuta/pb"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -91,6 +93,16 @@ func runGatewayServer(config util.Config, store db.Store) {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
+
+	statikFS, err := fs.New()
+
+	if err != nil {
+		log.Fatal("cannot create statik fs")
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HttpServerAddress)
 	if err != nil {
