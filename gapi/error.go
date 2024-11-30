b8aead673cb2525eb02775124eb3581510b926c8
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-11-30 07:25:45 +0100

    add authentication to update user grpc api

--- gapi/error.go
+++ gapi/error.go
@@ -23,3 +23,7 @@ func invalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) er
 	}
 	return statusDetails.Err()
 }
+
+func unauthenticatedError(error error) error {
+	return status.Errorf(codes.Unauthenticated, "unauthorized: %s", error)
+}
